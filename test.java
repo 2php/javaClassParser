eb4e45d904e1bf5ff2ec2875936e3a41947fb560
cherukus <srivikas99@gmail.com>
2019-10-11 07:44:10 -0400

    Made test class

---
+++ test.java
@@ -0,0 +1,10 @@
+class Test
+{
+    public String str = "yes";
+    private int num = 0xAABBCCDD;
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
